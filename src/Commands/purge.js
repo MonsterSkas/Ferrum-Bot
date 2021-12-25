afc6d63d45f0d1be8c7916e286d8a4cd22bb571c
Spandan Dey <Skyperflames@gmail.com>
2021-12-25 09:16:58 +0530

    Update purge.js

--- src/Commands/purge.js
+++ src/Commands/purge.js
@@ -3,25 +3,35 @@ module.exports = {
     name: 'purge',
     description: 'Bulk delete messages',
 
-    async execute(message, args) {
+    async execute(message, args)
+    {
 
-        if (!message.member.permissions.has('MANAGE_MESSAGES' || 'ADMINISTRATOR')) {
+        if (!message.member.permissions.has('MANAGE_MESSAGES' || 'ADMINISTRATOR'))
+        {
             
             return message.reply(`You don't have the permission to purge messages`)
 
-        } else if (!args[0]) {
+        }
+        else if (!args[0])
+        {
             
             return message.reply('Please specify the number of messages you want to delete!')
 
-        } else if (isNaN(args[0])) {
+        }
+        else if (isNaN(args[0]))
+        {
 
             message.reply(`Do you even know, what are numbers, anyway?!!`)
 
-        } else if (args[0] >= 1000) {
+        }
+        else if (args[0] >= 1000)
+        {
 
             message.reply(`I cant delete more than 999 messages at a time, pls forgive me :/`)
 
-        } else {
+        }
+        else
+        {
 
             await message.channel.messages.fetch({ limit: args[0] }).then(messages => {
 
