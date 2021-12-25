e5d28defa7d58afaf6ff3abacb4c708c29ab957d
Spandan Dey <Skyperflames@gmail.com>
2021-12-25 14:40:30 +0530

    Update main.js

--- main.js
+++ main.js
@@ -239,7 +239,7 @@ client.on('messageCreate', (message) => {
 
 
 // LOGIN TO THE BOT
-client.login('OTE5MjAxNjg0MjEzODc4ODI0.YbSXRw.iPd17NAnhZxtrqrVbFdqSmVtGMA').then( () => {
+client.login(process.env.TOKEN).then( () => {
 
     client.user.setPresence({
 
