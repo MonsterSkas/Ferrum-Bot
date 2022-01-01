700ff6511c912789678347e97ffc6353e5312fa0
Spandan Dey <Skyperflames@gmail.com>
2022-01-01 12:34:21 +0530

    change a few files

--- Commands/sourcecode.js
+++ Commands/sourcecode.js
@@ -5,7 +5,10 @@ module.exports = {
 
     async execute(message, args) {
 
-        if(!message.member.permissions.has('ADMINISTRATOR')) return
+        if (!message.member.permissions.has('ADMINISTRATOR')) return
+        
+        if (!message.member.id == '665576173828440071') return
+        else if (!message.member.id == '833890521893437440') return
 
         message.reply('https://github.com/xrn-gaming/Ferrum-Bot')
     }
